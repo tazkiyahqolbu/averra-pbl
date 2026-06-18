e8093a58df7e56f66541488ad0bffeb09d73a665
tazkiyahqolbu <tazkiyahqolbu2@gmail.com>
2026-06-18 11:56:55 +0700

    fix: perbaiki penulisan namespace Schedule di routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
