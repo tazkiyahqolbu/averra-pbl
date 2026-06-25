875abd02dc71c73707e19b232ba7d43848132f68
tazkiyahqolbu <tazkiyahqolbu2@gmail.com>
2026-06-25 22:18:16 +0700

    feat(profile): tambah ProfileController dengan method index

---
+++ app/Http/Controllers/User/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $user = auth()->user();
+        return view('user.profile.index', compact('user'));
+    }
+}
