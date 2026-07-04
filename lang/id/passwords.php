28f57fe44edeeabc5252a8211ccddaf512774afe
LeopardTD <zikrarevanzha@gmail.com>
2026-07-04 12:22:27 +0700

    Localization: Terjemahkan pesan default auth & validasi Laravel ke Bahasa Indonesia (lang/id)

---
+++ lang/id/passwords.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'reset'     => 'Kata sandi Anda telah berhasil direset.',
+    'sent'      => 'Kami telah mengirimkan tautan reset kata sandi ke email Anda.',
+    'throttled' => 'Harap tunggu sebelum mencoba kembali.',
+    'token'     => 'Token reset kata sandi ini tidak valid atau sudah kadaluarsa.',
+    'user'      => 'Kami tidak dapat menemukan pengguna dengan alamat email tersebut.',
+
+];
